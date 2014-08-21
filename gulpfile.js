e0043477239e3eb986643de2530c1c8b95f559fc
Andrew Ingram <andy@andrewingram.net>
2014-08-21 12:06:14 +0100

    Start work on the icon font

--- gulpfile.js
+++ gulpfile.js
@@ -3,47 +3,18 @@
 var fs = require('fs');
 var path = require('path');
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var duration = require('gulp-duration');
-var browserify = require('browserify');
-var envify = require('envify/custom');
-var mkpath = require('mkpath');
-var glob = require('glob');
-var watchify = require('watchify');
-var reactify = require('reactify');
-var prefix = require('gulp-autoprefixer');
-var sass = require('gulp-sass')
 
-var baseScripts = 'mediacat/static/mediacat/js/src/';
-var baseBuild = 'mediacat/static/mediacat/js/build/';
+var iconfont = require('gulp-iconfont');
 
-var paths = {
-  tests: baseScripts + '/**/__tests__/**/*.{js,jsx}',  
-  scripts: baseScripts + '/**/*.{js,jsx}',
-  entryScripts: baseScripts + '/**/init.{js,jsx}',
-  scss: 'static/css/',
-  cssOutput: 'mediacat/static/mediacat/css/'
-};
-
-var reactifyES6 = function(file) {
-  return reactify(file, {
-    es6: true,
-    extension: ['jsx', 'js']
-  });
-};
-
-gulp.task('sass', function () {
-    gulp.src(paths.scss+ '*.scss')
-        .pipe(sass())
-        .pipe(prefix("last 1 version", "> 1%", "ie 9"))
-        .pipe(gulp.dest(paths.cssOutput));
-});
-
-gulp.task("watch:css", function() {
-  return gulp.watch(paths.scss + '*.scss', ['sass']);
-});
-
-gulp.task("watch", function() {
-  gulp.run("watch:css");
-  gulp.run("watch:js");
+gulp.task('icon', function(){
+  gulp.src(['static/fonts/mediacat/svg/*.svg'])
+    .pipe(iconfont({
+      fontName: 'mediacat-icons', // required
+      appendCodepoints: true // recommended option
+    }))
+      .on('codepoints', function(codepoints, options) {
+        // CSS templating, e.g.
+        console.log(codepoints, options);
+      })
+    .pipe(gulp.dest('static/fonts/mediacat/'));
 });
