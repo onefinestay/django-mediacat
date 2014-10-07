0ae7d506438c902616edd847c084f6b283c7d682
Andrew Ingram <andy@andrewingram.net>
2014-10-07 13:33:19 +0100

    Set rating for selected image via 1,2,3,4,5 or 0 (reject)

--- mediacat/static/mediacat/library.js
+++ mediacat/static/mediacat/library.js
@@ -3885,10 +3885,44 @@
 	    var keyboard = this.getKeyboard();
 	    var flux = this.getFlux();
 	
+	    keyboard.on('1', this.setRating.bind(this, 1));
+	    keyboard.on('2', this.setRating.bind(this, 2));
+	    keyboard.on('3', this.setRating.bind(this, 3));
+	    keyboard.on('4', this.setRating.bind(this, 4));
+	    keyboard.on('5', this.setRating.bind(this, 5));
+	    keyboard.on('0', this.setRating.bind(this, 0));
 	    keyboard.on('up', this.cursorUp);
 	    keyboard.on('down', this.cursorDown);
 	    keyboard.on('left', this.cursorLeft);
 	    keyboard.on('right', this.cursorRight);
+	  },
+	
+	  componentWillUnmount: function() {
+	    window.removeEventListener('resize', this.updateDOMDimensions);
+	
+	    if (this.state.observer) {
+	      this.state.observer.disconnect();
+	    }
+	
+	    var keyboard = this.getKeyboard();
+	
+	    keyboard.off('1');
+	    keyboard.off('2');
+	    keyboard.off('3');
+	    keyboard.off('4');
+	    keyboard.off('5');
+	    keyboard.off('0');
+	    keyboard.off('up');
+	    keyboard.off('down');
+	    keyboard.off('left');
+	    keyboard.off('right');
+	  },
+	
+	  setRating: function(rating, event) {
+	    var selected = this.getFlux().store('Media').getSelectedMedia();
+	    if (selected) {
+	      this.getFlux().actions.media.setRating(selected, rating);
+	    }
 	  },
 	
 	  cursorUp: function() {
@@ -3963,21 +3997,6 @@
 	      }
 	      this.getFlux().actions.media.select(media.get(newIndex));
 	    }
-	  },  
-	
-	  componentWillUnmount: function() {
-	    window.removeEventListener('resize', this.updateDOMDimensions);
-	
-	    if (this.state.observer) {
-	      this.state.observer.disconnect();
-	    }
-	
-	    var keyboard = this.getKeyboard();
-	
-	    keyboard.off('up');
-	    keyboard.off('down');
-	    keyboard.off('left');
-	    keyboard.off('right');
 	  },  
 	
 	  setSort: function(option) {
