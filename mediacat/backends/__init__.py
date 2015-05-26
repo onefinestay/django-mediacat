5a3372ea0a71325ba957062091e714867c8b9f65
Andrew Ingram <andy@andrewingram.net>
2015-05-26 13:38:37 +0100

    future-proof import_string

--- mediacat/backends/__init__.py
+++ mediacat/backends/__init__.py
@@ -1,12 +1,16 @@
 from django.conf import settings
-from django.utils.module_loading import import_by_path
+
+try:
+    from django.utils.module_loading import import_string
+except ImportError:
+    from django.utils.module_loading import import_by_path as import_string
 
 
 DEFAULT_BACKEND = 'mediacat.backends.thumbor.Backend'
 
 
 def get_backend():
-    return import_by_path(getattr(
+    return import_string(getattr(
         settings,
         'MEDIACAT_THUMBNAIL_BACKEND',
         DEFAULT_BACKEND
